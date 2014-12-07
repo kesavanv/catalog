8cd97395e174e414070effb98edb45b79b044a3d
Varatharajan, Kesavan <kvaratharajan@paypal.com>
2014-12-07 18:05:16 +0530

    updated look n feel

--- data.js
+++ data.js
@@ -1,22 +1,29 @@
 var resources = {
-    "coding": [{
-        "language": "Express",
-        "topic": "Express",
-        "url" : "http://expressjs.com/"
-    },
-    {
-        "language": "NodeJs",
-        "topic": "NodeJs",
-        "url": "http://www.nodebeginner.org/"
-    },
-    {
-        "language": "BackboneJs",
-        "topic": "BackboneJs",
-        "url": "http://addyosmani.github.io/backbone-fundamentals/"
-    },
-    {
-        "language": "javascript",
-        "topic": "Design Patterns",
-        "url" : "http://addyosmani.com/resources/essentialjsdesignpatterns/book/"
-    }]
+    "coding": [
+        {
+            "topic": "javascript",
+            "description": "Learning JavaScript Design Patterns",
+            "url" : "http://addyosmani.com/resources/essentialjsdesignpatterns/book/"
+        },
+        {
+            "topic": "NodeJs",
+            "description": "The Node Beginner Book » A comprehensive Node.js tutorial",
+            "url": "http://www.nodebeginner.org/"
+        },
+        {
+            "topic": "BackboneJs",
+            "description": "Developing Backbone.js Applications",
+            "url": "http://addyosmani.github.io/backbone-fundamentals/"
+        },
+        {
+            "topic": "Express",
+            "description": "Node.js web application framework",
+            "url" : "http://expressjs.com/"
+        },
+        {
+            "topic": "Go",
+            "description": 'An Introduction to Programming in Go',
+            "url": "http://www.golang-book.com/"
+        }
+    ]
 };
