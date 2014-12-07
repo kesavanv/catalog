aad9061f09a01606ee760fd8b9b60a44aa02bdd8
Varatharajan, Kesavan <kvaratharajan@paypal.com>
2014-12-07 17:06:12 +0530

    updated boilerplate

---
+++ data.js
@@ -0,0 +1,42 @@
+var data = {
+    "users": [ {
+        "person": {
+            "firstName": "Garry",
+            "lastName": "Finch"
+        },
+        "jobTitle": "Front End Technical Lead",
+        "twitter": "gazraa"
+    }, {
+        "person": {
+            "firstName": "Garry",
+            "lastName": "Finch"
+        },
+        "jobTitle": "Photographer",
+        "twitter": "photobasics"
+    }, {
+        "person": {
+            "firstName": "Garry",
+            "lastName": "Finch"
+        },
+        "jobTitle": "LEGO Geek",
+        "twitter": "minifigures"
+    } ]
+};
+
+var resources = {
+    "coding": [{
+        "language": "Express",
+        "topic": "Express",
+        "url" : "http://expressjs.com/"
+    },
+    {
+        "language": "NodeJs",
+        "topic": "NodeJs",
+        "url": "http://www.nodebeginner.org/"
+    },
+    {
+        "language": "javascript",
+        "topic": "Design Patterns",
+        "url" : "http://addyosmani.com/resources/essentialjsdesignpatterns/book/"
+    }]
+};
